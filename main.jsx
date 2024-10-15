569971144f3bd85f60b1ff5a63683fb513badc3d
lotickles <148078790+lotickles@users.noreply.github.com>
2024-10-15 16:45:59 +0800

    still fixing it

--- main.jsx
+++ main.jsx
@@ -2,19 +2,19 @@ import 'antd/dist/reset.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { persistor, store } from 'redux/store';
+import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import App from 'components/App';
-import { GlobalStyles } from 'styles/GlobalStyles';
+import App from './components/App';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/expense-tracker">
+  <BrowserRouter basename="/goit-expenses-tracker">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
